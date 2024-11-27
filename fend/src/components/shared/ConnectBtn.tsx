b1b62b2a4168653e90ae2fed5cf61844758f4a08
lottaverse <lottaverse.io@gmail.com>
2024-11-27 03:53:35 -0800

    change usdt icon

--- fend/src/components/shared/ConnectBtn.tsx
+++ fend/src/components/shared/ConnectBtn.tsx
@@ -118,13 +118,13 @@ export const ConnectBtn = () => {
                           marginRight: 4,
                         }}
                       >
-                        {chain.iconUrl && (
+                        {/* {chain.iconUrl && (
                           // <img
                           //   alt={chain.name ?? "Chain icon"}
                           //   src={chain.iconUrl}
                           //   style={{ width: 12, height: 12 }}
                           // />
-                        )}
+                        )} */}
                       </div>
                     )}
                     {/* {chain.name}  */}
