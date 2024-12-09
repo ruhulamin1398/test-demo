d1aec062627f8873d6f760731aeff9c6297c6ba5
ruhulamin1398 <ruhulamin010398@gmail.com>
2024-12-09 20:53:50 +0600

    changes tites

--- backend/controllers/blockchainContoller.js
+++ backend/controllers/blockchainContoller.js
@@ -276,9 +276,9 @@ module.exports = {
 
 
     //  1. Find the latest Easy lottery from Blockchain
-    const lotteryDAta = await upgradeLotteryTicketCount(0); 
-    const lotteryDAta2 =  await upgradeLotteryTicketCount(1); 
+    const easyLottery = await upgradeLotteryTicketCount(0); 
+    const superLottery =  await upgradeLotteryTicketCount(1); 
 
-    res.status(200).json({lotteryDAta,lotteryDAta2});
+    res.status(200).json({easyLottery,superLottery});
   },
 };
