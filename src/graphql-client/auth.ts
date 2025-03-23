3a6d41ab33b9857b0d7733987079358d0b2536ad
ruhulamin1398 <ruhul.ok@gmail.com>
2025-03-24 04:23:49 +0800

    Client side graphql

--- src/graphql-client/auth.ts
+++ src/graphql-client/auth.ts
@@ -1,55 +1,103 @@
 import { gql } from "@apollo/client";
 
+// LOGIN_MUTATION: Handles login via email and password
 export const LOGIN_MUTATION = gql`
-  mutation Login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       user {
         id
+        name
+        email
         firstName
         lastName
         phoneNumber {
           countryCode
           number
         }
-        email
-        username
+        avatar
         role
+        authProvider
+        socialId
       }
     }
   }
 `;
 
+// REGISTER_MUTATION: Handles user registration
 export const REGISTER_MUTATION = gql`
   mutation CreateUser(
+    $name: String!
     $email: String!
-    $username: String!
     $password: String!
-    $firstName: String!
-    $lastName: String!
-    $phoneNumber: PhoneNumberInput!
+    $firstName: String
+    $lastName: String
+    $phoneNumber: PhoneNumberInput
   ) {
     createUser(
+      name: $name
       email: $email
       password: $password
       firstName: $firstName
       lastName: $lastName
       phoneNumber: $phoneNumber
-      username: $username
     ) {
       user {
         id
+        name
+        email
         firstName
         lastName
         phoneNumber {
           countryCode
           number
         }
-        email
-        username
+        avatar
+        role
+        authProvider
+        socialId
       }
     }
   }
 `;
+
+// SOCIAL_LOGIN_MUTATION: Handles social login (Google, Facebook, etc.)
+export const SOCIAL_LOGIN_MUTATION = gql`
+  mutation SocialLogin(
+    $socialId: String!
+    $email: String!
+    $firstName: String
+    $lastName: String
+    $phoneNumber: PhoneNumberInput
+    $authProvider: AuthProviderEnum!
+  ) {
+    socialLogin(
+      socialId: $socialId
+      email: $email
+      firstName: $firstName
+      lastName: $lastName
+      phoneNumber: $phoneNumber
+      authProvider: $authProvider
+    ) {
+      user {
+        id
+        name
+        email
+        firstName
+        lastName
+        phoneNumber {
+          countryCode
+          number
+        }
+        avatar
+        role
+        authProvider
+        socialId
+      }
+    }
+  }
+`;
+
+// LOGOUT_MUTATION: Handles user logout
 export const LOGOUT_MUTATION = gql`
   mutation Logout {
     logout {
@@ -58,9 +106,73 @@ export const LOGOUT_MUTATION = gql`
   }
 `;
 
+// ME_QUERY: Fetches the authenticated user's details
 export const ME_QUERY = gql`
   query Me {
     me {
+      id
+      name
+      email
+      firstName
+      lastName
+      phoneNumber {
+        countryCode
+        number
+      }
+      avatar
+      role
+      authProvider
+      socialId
+    }
+  }
+`;
+
+// GET_USERS_QUERY: Fetches a list of users with pagination and optional filters
+export const GET_USERS_QUERY = gql`
+  query GetUsers($page: PaginationInput!, $filter: UserFilterInput) {
+    getUsers(page: $page, filter: $filter) {
+      users {
+        id
+        name
+        email
+        firstName
+        lastName
+        phoneNumber {
+          countryCode
+          number
+        }
+        avatar
+        role
+        authProvider
+        socialId
+      }
+      totalCount
+    }
+  }
+`;
+
+// UPDATE_PROFILE_AVATAR_MUTATION: Updates the user's avatar
+export const UPDATE_PROFILE_AVATAR_MUTATION = gql`
+  mutation UpdateProfileAvatar($avatarUrl: String!) {
+    updateProfileAvatar(avatarUrl: $avatarUrl) {
+      id
+      avatar
+    }
+  }
+`;
+
+// UPDATE_GENERAL_INFO_MUTATION: Updates the user's general information (firstName, lastName, phoneNumber)
+export const UPDATE_GENERAL_INFO_MUTATION = gql`
+  mutation UpdateGeneralInfo(
+    $firstName: String
+    $lastName: String
+    $phoneNumber: PhoneNumberInput
+  ) {
+    updateGeneralInfo(
+      firstName: $firstName
+      lastName: $lastName
+      phoneNumber: $phoneNumber
+    ) {
       id
       firstName
       lastName
@@ -68,9 +180,39 @@ export const ME_QUERY = gql`
         countryCode
         number
       }
-      email
-      username
-      role
+    }
+  }
+`;
+
+// RESET_PASSWORD_MUTATION: Resets the user's password by email
+export const RESET_PASSWORD_MUTATION = gql`
+  mutation ResetPassword($email: String!) {
+    resetPassword(email: $email) {
+      success
+      message
+    }
+  }
+`;
+
+// CHANGE_PASSWORD_MUTATION: Changes the user's password
+export const CHANGE_PASSWORD_MUTATION = gql`
+  mutation ChangePassword($currentPassword: String!, $newPassword: String!) {
+    changePassword(
+      currentPassword: $currentPassword
+      newPassword: $newPassword
+    ) {
+      success
+      message
+    }
+  }
+`;
+
+// DELETE_USER_MUTATION: Deletes a user account
+export const DELETE_USER_MUTATION = gql`
+  mutation DeleteUser($id: ID!) {
+    deleteUser(id: $id) {
+      success
+      message
     }
   }
 `;
