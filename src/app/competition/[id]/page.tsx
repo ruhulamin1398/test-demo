b86f0c892d769b3b5b0fa5330bd306e1b1e84790
ruhulamin1398 <ruhul.ok@gmail.com>
2025-05-13 00:34:24 +0600

    uplode logo and fav

--- src/app/competition/[id]/page.tsx
+++ src/app/competition/[id]/page.tsx
@@ -1,6 +1,9 @@
-import React from "react"; 
+import React from "react";
 import { SiingleCompetitionView } from "@/sections/competition-single/view";
+import { Metadata } from "next";
+import { CONFIG } from "@/global-config";
 
+export const metadata: Metadata = { title: `Competitions - ${CONFIG.appName}` };
 export default async function HomePage() {
   return (
     <div>
